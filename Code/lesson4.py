98fc976b77d8fafc61a07cb2d33dba192e4f2fd8
juna <juna@debian>
2017-03-18 13:16:47 +0300

    Add presentation list

--- Code/lesson4.py
+++ Code/lesson4.py
@@ -53,7 +53,46 @@ class mylist:
         b.set_value(x)
         b.set_next(a.get_next())
         a.set_next(b)
-        
- L=mylist(); L.add(1);  L.add(2);L.add(3);
- L.print_list();L.put_into(2,6);
+
+
+    def search(self,x):
+        current = self.__first
+        found = False
+        while (current != None) and (not found):
+            if current.get_value() == x:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def count(self):
+        a = self.__first
+        count = 1
+        while a.get_next() != None:
+            a = a.get_next()
+            count += 1
+        return count
+
+    def buble(self):
+        for i in range(L.count()):
+            a = self.__first
+            b = self.__first.get_next()
+            while b != None:
+                if a.get_value() > b.get_value():
+                    i = a.get_value()
+                    j = b.get_value()
+                    a.set_value(j)
+                    b.set_value(i)
+                else:
+                    a = a.get_next()
+                    b = b.get_next()
+
+L=mylist()
+L.add(1)
+L.add(9)
+L.add(3)
+L.add(45)
+L.add(13)
+L.add(27)
+L.print_list()
  
