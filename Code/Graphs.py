53e698d4a40def3be46783aaa9613d05b4fdf396
juna <juna@debian>
2017-12-08 08:40:04 +0300

    add Graphs.py

--- Code/Graphs.py
+++ Code/Graphs.py
@@ -1,14 +1,16 @@
 class node():
     __value = None
     __links = None
-    __lable=None
-    __d=None
+    lable=None
+    d=None
+    ways=None
     
     def __init__(self, n):
         self.__value = n
         self.__links = []
-        self.__lable=False
-        self.__d=None
+        self.lable=False
+        self.d=None
+        self.ways=[]
         
     def set_value(self, n):
         self.__value = n
@@ -98,36 +100,80 @@ class graph():
 
    
             
-
-
-def deykstra(self,current_node):
+    def deykstra(self,current_node):
         for i in self.get_list_nodes():
-            i.__lable=False
-            i.__d=None
-        current_node.__lable=True
-        current_node.__d=0
+            i.lable=False
+            i.d=None
+        current_node.lable=True
+        current_node.d=0
         pr=True
         while pr:
             pr=False
             for i in self.get_list_nodes():
-                if i.__ lable==False:
+                if i.lable==False:
                     pr=True
                     break
             if pr:
                 for i in  current_node.get_near_links():
-                    if  i.__lable==False:
-                        if i.__d==None:
-                            i.__d=current_node.__d+current_node.get_weight(i)
+                    if  i.lable==False:
+                        if i.d==None:
+                            i.ways.append(current_node.get_value())
+                            i.d=current_node.d+current_node.get_weight(i)
                         else:
-                            if i.__d>current_node.__d+current_node.get_weight(i):
-                                 i.__d=current_node.__d+current_node.get_weight(i)
-                for i in
-            
+                            if i.d>current_node.d+current_node.get_weight(i):
+                                i.ways.append(current_node.get_value())
+                                i.d=current_node.d+current_node.get_weight(i)
+                mind=None;
+
                 
+                
+                for i in self.get_list_nodes():
+                    if (i.lable==False)&(i.d!=None):
+                        if mind==None:
+                            mind=i.d
+                            current_node=i
+                        elif (mind>i.d):
+                            mind=i.d
+                            current_node=i
+                current_node.ways.append(current_node.get_value())
+                current_node.lable=True
+                            
+    def print_d(self):
+         for i in self.get_list_nodes():
+             print("Вершина ",i.get_value()," : ",i.d)
+    def prima(self, cur_node):
+        V=[cur_node]
+        R=[]
+        R1=[]
+        while (len(V)!=len(self.__list_nodes)):
+            near=cur_node.get_links()
+            min_w=float("inf")
+            for i in near:
+                if not(i[0] in V) and i[1]!=None:
+                    R1.append([[cur_node.get_value(),i[0].get_value()], i[1], i[0]])
+            print("Проход")
+            for i in R1:
+                print(i[0])
+            for i in range(len(R1)):
+                if min_w>R1[i][1]:
+                    min_w=R1[i][1]
+                    min_node=R1[i][2]
+                    min_index=i
+            R.append([cur_node.get_value(),min_node.get_value()])
+            del R1[min_index]        
+            
+            V.append(min_node)
+            cur_node=min_node
+        print(R)    
+            
+                    
+               
             
 G = graph()
-G.create_from_adjacency([[None, 2, 10,None, None], [None, None, 3, -1, 7], [None, None, None, 5, None], [0, None, None, None, 1], [1, None, 12, None, None]])
-#G.vizualize()
+G1= graph()
+G.create_from_adjacency([[None, 2, 10,None, None], [None, None, 3, 1, 7], [None, None, None, 5, None], [0, None, None, None, 1], [1, None, 12, None, None]])
+G1.create_from_adjacency([[None, 2, 10, 0, 1], [2, None, 3, 1, 7], [10, 3, None, 5, 12], [0, 1, 5, None, 1], [1,7, 12, 1, None]])
+G1.vizualize()
 A=G.get_list_nodes()[0]
 #G.round_width(A)
 
